b2fbf07df5756dbbbe4a3d79cb8e3f39a5ebfdad
M.Ali Hamza <mohammadalihamza97@gmail.com>
2026-07-23 15:00:50 +0500

    Configuration for Extracter

---
+++ config.py
@@ -0,0 +1,14 @@
+"""
+CONFIGURATION FOR THE GIT PROFILE EXTRACTION SETUP
+"""
+
+
+
+#Base Url
+BASE_URL = "https://github.com/"
+
+
+#Files Path
+JSON_PATH = "/data/profile.json"
+
+CSV_PATH = "/data/profile.csv"
